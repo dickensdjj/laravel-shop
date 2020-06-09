1ad075a4025cd51e2e9e4fd5dd3174a1226cdaaa
Dickens Zihua Fan <dickensdjj@gmail.com>
2020-06-09 23:44:08 +1000

    - adding basic layout, adding server js and bootstrap from npm install, making use of laravel mix

---
+++ bootstrap/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function test_helper()
+{
+    return 'Ok';
+}
+
+//function route_class()
+//{
+//    return str_replace('.', '-', Route::current()->getName());
+//}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            Design By <a href="#" target="_blank">Leo</a> Dickens Zihua Fan <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:dickensdjj@gmail.com">contact us</a></p>
+    </div>
+</footer>
